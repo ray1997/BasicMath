eca2bbcf8361a610dcfb4459297ce26269ba0271
Toon Werawat <14814401+ray1997@users.noreply.github.com>
2022-12-06 14:12:53 +0700

    change all method to expression

--- MathOperations.cs
+++ MathOperations.cs
@@ -2,32 +2,12 @@
 {
     public static class MathOperations<T>
     {
-        public static T Add(T a, T b)
-        {
-            dynamic x = a;
-            dynamic y = b;
-            return x + y;
-        }
+        public static T Add(T a, T b) => a + b;
 
-        public static T Subtract(T a, T b)
-        {
-            dynamic x = a;
-            dynamic y = b;
-            return x - y;
-        }
+        public static T Subtract(T a, T b) => a - b;
 
-        public static T Multiply(T a, T b)
-        {
-            dynamic x = a;
-            dynamic y = b;
-            return x * y;
-        }
+        public static T Multiply(T a, T b) => a * b;
 
-        public static T Divide(T a, T b)
-        {
-            dynamic x = a;
-            dynamic y = b;
-            return x / y;
-        }
+        public static T Divide(T a, T b) => a / b;
     }
 }
