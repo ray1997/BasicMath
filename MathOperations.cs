d12ab46e4258b81d4448ca4c802a1c4cea67f623
Toon Werawat <14814401+ray1997@users.noreply.github.com>
2022-12-06 14:05:30 +0700

    Prompt: "Write a c# generic class that does basic math operation. Add, Subtract, Multiply, and Divide"

---
+++ MathOperations.cs
@@ -0,0 +1,30 @@
+﻿public class MathOperations<T>
+{
+    public T Add(T a, T b)
+    {
+        dynamic x = a;
+        dynamic y = b;
+        return x + y;
+    }
+
+    public T Subtract(T a, T b)
+    {
+        dynamic x = a;
+        dynamic y = b;
+        return x - y;
+    }
+
+    public T Multiply(T a, T b)
+    {
+        dynamic x = a;
+        dynamic y = b;
+        return x * y;
+    }
+
+    public T Divide(T a, T b)
+    {
+        dynamic x = a;
+        dynamic y = b;
+        return x / y;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿MathOperations<int> mathOperations = new MathOperations<int>();
+
+int result1 = mathOperations.Add(5, 10);
+int result2 = mathOperations.Subtract(20, 10);
+int result3 = mathOperations.Multiply(5, 10);
+int result4 = mathOperations.Divide(100, 10);
+
+Console.WriteLine($"5 + 10 = {result1}");
+Console.WriteLine($"20 - 10 = {result2}");
+Console.WriteLine($"5 × 10 = {result3}");
+Console.WriteLine($"100 ÷ 10 = {result4}");
