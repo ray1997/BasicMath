9c1b179acb6f4eaddb1f0561e3f8e553e37ee97d
Toon Werawat <14814401+ray1997@users.noreply.github.com>
2022-12-06 14:06:31 +0700

    Make it static class

--- MathOperations.cs
+++ MathOperations.cs
@@ -1,27 +1,27 @@
-﻿public class MathOperations<T>
+﻿public static class MathOperations<T>
 {
-    public T Add(T a, T b)
+    public static T Add(T a, T b)
     {
         dynamic x = a;
         dynamic y = b;
         return x + y;
     }
 
-    public T Subtract(T a, T b)
+    public static T Subtract(T a, T b)
     {
         dynamic x = a;
         dynamic y = b;
         return x - y;
     }
 
-    public T Multiply(T a, T b)
+    public static T Multiply(T a, T b)
     {
         dynamic x = a;
         dynamic y = b;
         return x * y;
     }
 
-    public T Divide(T a, T b)
+    public static T Divide(T a, T b)
     {
         dynamic x = a;
         dynamic y = b;
--- Program.cs
+++ Program.cs
@@ -1,9 +1,7 @@
-﻿MathOperations<int> mathOperations = new MathOperations<int>();
-
-int result1 = mathOperations.Add(5, 10);
-int result2 = mathOperations.Subtract(20, 10);
-int result3 = mathOperations.Multiply(5, 10);
-int result4 = mathOperations.Divide(100, 10);
+﻿int result1 = MathOperations<int>.Add(5, 10);
+int result2 = MathOperations<int>.Subtract(20, 10);
+int result3 = MathOperations<int>.Multiply(5, 10);
+int result4 = MathOperations<int>.Divide(100, 10);
 
 Console.WriteLine($"5 + 10 = {result1}");
 Console.WriteLine($"20 - 10 = {result2}");
