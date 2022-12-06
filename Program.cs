5fb65c6c471ca463403ab1a4922629432c3015ba
Toon Werawat <14814401+ray1997@users.noreply.github.com>
2022-12-06 14:14:08 +0700

    Update example

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
-﻿int result1 = MathOperations<int>.Add(5, 10);
-int result2 = MathOperations<int>.Subtract(20, 10);
-int result3 = MathOperations<int>.Multiply(5, 10);
-int result4 = MathOperations<int>.Divide(100, 10);
+﻿int result1 = BasicMath.MathOperations.Add(5, 10);
+int result2 = BasicMath.MathOperations.Subtract(20, 10);
+int result3 = BasicMath.MathOperations.Multiply(5, 10);
+int result4 = BasicMath.MathOperations.Divide(100, 10);
 
 Console.WriteLine($"5 + 10 = {result1}");
 Console.WriteLine($"20 - 10 = {result2}");
